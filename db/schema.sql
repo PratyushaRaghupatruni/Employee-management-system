9456bde12de196e7a42d370eb33b9eb893df74ce
pratyu <pratyusha.raghupatruni@gmail.com>
2020-09-10 01:52:28 -0700

    created database in schema.sql and inserted values in seed.sql

---
+++ db/schema.sql
@@ -0,0 +1,58 @@
+-- Drops the employee_db if it exists currently --
+DROP DATABASE IF EXISTS employee_db;
+-- Creates the "employee_db" database --
+CREATE DATABASE employee_db;
+
+-- Makes it so all of the following code will affect employee_db --
+USE employee_db;
+
+CREATE TABLE department(
+    id INTEGER auto_increment NOT NULL,
+
+    name VARCHAR(30),
+
+    PRIMARY key(id)
+);
+
+CREATE TABLE role(
+    id INTEGER auto_increment NOT NULL,
+
+    title VARCHAR(30),
+
+    salary DECIMAL,
+
+  department_id INTEGER,
+
+  CONSTRAINT fk_department FOREIGN KEY(department_id)
+  REFERENCES department(id) 
+  ON UPDATE CASCADE
+  ON DELETE CASCADE,
+
+   PRIMARY KEY(id)
+
+);
+-- Creates the table "people" within employee_db --
+CREATE TABLE employee (
+
+  id INTEGER auto_increment not null,
+
+  first_name VARCHAR(30),
+
+  last_name VARCHAR(30),
+
+  role_id INTEGER,
+
+  CONSTRAINT fk_role FOREIGN KEY(role_id)
+  REFERENCES role(id)
+  ON UPDATE CASCADE
+  ON DELETE CASCADE,
+
+  manager_id INTEGER,
+
+  CONSTRAINT fk_employee FOREIGN KEY(manager_id)
+  REFERENCES employee(id)
+  ON UPDATE CASCADE
+  ON DELETE CASCADE,
+
+  PRIMARY KEY(id)
+);
---
+++ db/seed.sql
@@ -0,0 +1,19 @@
+use employee_db;
+INSERT into department(name) values ('Sales'),('Engineering'),('Finance'),('Legal');
+INSERT INTO role(title,salary) VALUES ('Sales Lead',100000),
+                                      ('SalesPerson',80000), 
+                                      ('Lead Engineer',150000),
+                                      ('Software Engineer',120000),
+                                      ('Accountant',125000,
+                                      ('Legal Team Lead',250000),
+                                      ('Lawyer',1900000),
+                                      ('Software Engineer',175000);
+INSERT INTO employee(first_name,last_name) values ('John','Doe'),
+                                                  ('Mike','Chan'),
+                                                  ('Ashley','Rodriquez'),
+                                                   ('Kevin','Tupik'),
+                                                   ('Malia','Brown'),
+                                                   ('Sarah','Lourd'),
+                                                   ('tom','Allen'),
+                                                   ('Christian','Tammer');                       
+
