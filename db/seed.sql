c06204a0af0584b19a0761545577b2196b63497e
pratyu <pratyusha.raghupatruni@gmail.com>
2020-09-10 13:24:30 -0700

    added all the prompts in prompts.js

--- db/seed.sql
+++ db/seed.sql
@@ -1,14 +1,14 @@
 use employee_db;
 INSERT into department(name) values ('Sales'),('Engineering'),('Finance'),('Marketing'),('Legal');
-INSERT INTO role(title,salary,department_id) VALUES ('Sales Lead',100000,1),1
-                                      ('SalesPerson',80000,1), 2
-                                      ('Web Developer',150000,2),3
-                                      ('Project Manager',16000,2),4
-                                      ('Engineering Manager',125000,2),5
-                                      ('Research Analyst',250000,3),6
-                                      ('HR Manager',1900000,4),7
-                                      ('Recruiter',100000,4),8
-                                      ('Lawyer',175000,5);9
+INSERT INTO role(title,salary,department_id) VALUES ('Sales Lead',100000,1),
+                                      ('SalesPerson',80000,1), 
+                                      ('Web Developer',150000,2),
+                                      ('Project Manager',16000,2),
+                                      ('Engineering Manager',125000,2),
+                                      ('Research Analyst',250000,3),
+                                      ('HR Manager',1900000,4),
+                                      ('Recruiter',100000,4),
+                                      ('Lawyer',175000,5);
 INSERT INTO employee(first_name,last_name,role_id,manager_id) values 
                                                   ('Ashley','Rodriquez',1,NULL),
                                                    ('Kevin','Tupik',2,1)
--- prompt.js
+++ prompt.js
@@ -3,13 +3,13 @@ module.exports={
         {
             type:'list',
             name:'welcome',
-            message:'what would you like to do'
+            message:'what would you like to do',
             choices:[
                 'View all employees',
                 'View all employees by department',
                 'View all employees by manager',
                 'View all roles',
-                'View all departments',               
+                'View all departments',
                 'Add Employee',
                 'Add Department',
                 'Add Role',
@@ -19,7 +19,38 @@ module.exports={
                 'Exit'
             ]  
         },
-      
+    ],
+    addEmployee:[
+        {
+            type:'input',
+            name:'firstName',
+            message:'What is Employees First Name'
+        },
+        {
+          type:'input' ,
+          name:'lastName',
+          message:'What is Employees Last Name' 
+        },
+    ],
+    departmentData:[
+        { 
+            type:'input',
+            name:'addDepartment',
+            message:'What department would you like to add'
+
+        },
     ],
 
+    addRole:[
+        {
+          type:'input',
+          name:'roleTitle',
+          message:'What role do you want to add'
+    },
+    {
+        type:'input',
+        name:'roleSalary',
+        message:'What is expected the salary for this role'    
+    },
+    ],
 }
