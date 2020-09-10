b002a98de8e18cd42a0de71a552e9aad8eade91a
pratyu <pratyusha.raghupatruni@gmail.com>
2020-09-10 02:14:03 -0700

    added database connection in connection.js

--- db/connection.js
+++ db/connection.js
@@ -1,1 +1,14 @@
- 
+'use strict' ;
+const util=require('util');
+const mysql=require('mysql');
+
+const connection=mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'pratyushakamal',
+    database:employee_db
+});
+
+connection.connect();
+connection.query=util.promisify(connection.query);
+module.exports=connection;
